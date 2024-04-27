55d33c782ae66197f10b7ff998b7aff6b2e7c27d
iAmGiG <gigachris1@hotmail.com>
2024-04-27 17:02:44 -0400

    added some a print to make sure the server was running

--- Phase1_LZ4Compression/server.py
+++ Phase1_LZ4Compression/server.py
@@ -62,6 +62,7 @@ def main():
     """
     host = '0.0.0.0'
     port = 5000
+    print(f"Server is running and listening on port {port}")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((host, port))
